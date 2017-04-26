feb136e1a2fae241bffab747097ffabe3d0bd08e
Carlo Francisco <carlo.francisco@refinery29.com>
2017-04-26 19:02:34 -0400

    Wrap up homework for week 1

--- week1.hs
+++ week1.hs
@@ -10,7 +10,31 @@ toDigits n
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = reverse(toDigits n)
 
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft []          = []
+doubleEveryOtherFromLeft (x:[])      = [x]
+doubleEveryOtherFromLeft (x:(y:zs))  = x : y * 2 : doubleEveryOtherFromLeft(zs)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther n = reverse(doubleEveryOtherFromLeft(reverse n))
+
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:[]) = x
+sumDigits (x:(y:zs)) = sumDigits(toDigits(x)) + sumDigits(toDigits(y)) + sumDigits(zs)
+
+validate :: Integer -> Bool
+validate n  = mod
+                (sumDigits(doubleEveryOther(toDigits n)))
+                10
+              == 0
+
 main :: IO ()
 main = do
   print (toDigits 12340)
   print (toDigitsRev 256)
+  print (doubleEveryOther [8,7,6,5])
+  print (doubleEveryOther [1,2,3])
+  print (sumDigits [16,7,12,5])
+  print (validate 4012888888881881)
+  print (validate 4012888888881882)
