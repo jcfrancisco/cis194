3eb5c5c08daa3641920c1a188bf01f5ee99d76f1
Carlo Francisco <carlo.francisco@refinery29.com>
2017-06-23 15:16:13 -0400

    Problems 3 and 4 week5

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -14,3 +14,39 @@ evalParsedExp (Just i) = Just (eval i)
 
 evalStr :: String -> Maybe Integer
 evalStr = evalParsedExp . parseExp Lit Add Mul
+
+-- Expr is a "type class," all of whose implementations
+-- can do "lit", "add" and "mul"
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+-- Exprt is an instance of the Expr type class. Here's
+-- how it does "lit", "add", and "mul
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax a) (MinMax b) = MinMax (max a b)
+  mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit a = Mod7 ((max a 0) `mod` 7)
+  add (Mod7 a) (Mod7 b) = lit (lit a + lit b)
+  mul (Mod7 a) (Mod7 b) = lit (lit a * lit b)
--- week5/week5.hs
+++ week5/week5.hs
@@ -1,7 +1,21 @@
 import Calc
 import ExprT
+import Parser
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "5 * 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
 
 main :: IO ()
 main = do
-  print $ eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
-  print $ evalStr "(2+3)*4"
+--  print $ eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+--  print $ evalStr "(2+3)*4"
+  print $ testInteger
+  print $ testBool
+  print $ testMM
+  print $ testSat
